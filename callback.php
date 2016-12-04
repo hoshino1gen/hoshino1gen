ea09cc2dc6c7dab0ffd6409a901bf2d3e8c7e992
tk <ro99881d@gmail.com>
2016-12-04 17:28:39 +0900

    add new file7

--- callback.php
+++ callback.php
@@ -1,28 +1,32 @@
 <?php
-$json_string = file_get_contents('php://input');
-$jsonObj = json_decode($json_string);
-$to = $jsonObj->{"result"}[0]->{"content"}->{"from"};
+$request = file_get_contents("php://input");
+$json = json_decode($request);
+$content = $json->result[0]->content;
 
-// テキストで返事をする場合
-$response_format_text = ['contentType'=>1,"toType"=>1,"text"=>"hello"];
-// 画像で返事をする場合
-$response_format_image = ['contentType'=>2,"toType"=>1,'originalContentUrl'=>"画像URL","previewImageUrl"=>"サムネイル画像URL"];
-// 他にも色々ある
-// ....
+$header = array(
+    'Content-Type: application/json; charser=UTF-8',
+    'X-Line-ChannelID:1490797696 ',  // Channel ID
+    'X-Line-ChannelSecret: e6fb3e87be15eaf35b0b6d47eda99d20',  // ChannelID Secret
+    'X-Line-Trusted-User-With-ACL: ',  // MID
+);
+$post = array(
+    'to' => array($content->from),
+    'toChannel' => 1383378250,  // Fixed value.
+    'eventType' => '138311608800106203',  // Fixed value.
+    'content' => array(
+        'contentType' => 1,
+        'toType' => 1,
+    ),
+);
 
-// toChannelとeventTypeは固定値なので、変更不要。
-$post_data = ["to"=>[$to],"toChannel"=>"1383378250","eventType"=>"138311608800106203","content"=>$response_format_image];
+// ボットが返答する内容。ここでは送られた内容を復唱するだけ
+$post['content']['text'] = $content->text.' ですね';
+
+$post = json_encode($post);
 
 $ch = curl_init("https://trialbot-api.line.me/v1/events");
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json; charser=UTF-8',
-    'X-Line-ChannelID: 1490797696',
-    'X-Line-ChannelSecret:e6fb3e87be15eaf35b0b6d47eda99d20',
-    'X-Line-Trusted-User-With-ACL: 発行されたMID'
-    ));
 $result = curl_exec($ch);
-curl_close($ch);
