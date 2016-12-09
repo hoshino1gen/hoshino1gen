dfc4eea140ae73f71b67615615086457e1006c30
tk <ro99881d@gmail.com>
2016-12-09 18:47:11 +0900

    add 33

--- config.php
+++ config.php
@@ -2,6 +2,10 @@
 
 ini_set('display_errors', 1);
 
+ini_set('log_errors','On');
+ini_set('error_log','syslog');
+error_reporting(E_ALL);
+
 session_start();
 
 require_once(__DIR__ . '/functions.php');
