f5801aa661e9b875587b1f019efca92446761cc7
tk <ro99881d@gmail.com>
2016-12-11 14:48:41 +0900

    add 1026

--- index.php
+++ index.php
@@ -165,7 +165,7 @@ else :
 
 
 
-    $TextMessageBuilder = new TextMessageBuilder("はいはい、聞いてるぜ");
+    $TextMessageBuilder = new TextMessageBuilder("はいはい、起きてるぜ");
 //    $TextMessageBuilder1 = new TextMessageBuilder("出勤を記録しました。");
 //    $TextMessageBuilder2 = new TextMessageBuilder( date( "Y年m月d日 H時i分" ) );
 
